62f2365c38b233c91ebbec5e7d12699789fdc193
Ashok Dey <ashokdey100@gmail.com>
2018-04-15 08:48:45 +0530

    --update : added CORS support

--- server/app.js
+++ server/app.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const graphQLHTTP = require('express-graphql');
 
 const schema = require('./schema');
 require('./db');
 // Assign a port for the app to listen
 const PORT = 5050;
-
 const app = express();
 
+// allow CORS
+app.use(cors());
 // use the graphql middleware to accept ql queries
 app.use('/ql', graphQLHTTP({
   schema,
