061cb683ebbe8ee6822a70bb25bfb37a5891a4b5
G <igauravsehrawat@gmail.com>
2018-04-16 21:04:19 +0530

    Typo fix: 😎

--- server/models/AuthorModel.js
+++ server/models/AuthorModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const AuthorSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'Nmae of author is required'],
+    required: [true, 'Name of author is required'],
   },
   age: { type: Number },
 });
