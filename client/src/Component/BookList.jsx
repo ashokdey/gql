690c4ee73ae0390f84f7ccf996f630d2a9cbedd2
Ashok Dey <ashokdey100@gmail.com>
2018-04-17 17:57:48 +0530

    --update : BookList skeleton

---
+++ client/src/Component/BookList.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class BookList extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ul id="book-list">
+          <li>Book Name</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default BookList;
